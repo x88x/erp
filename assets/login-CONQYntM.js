a3842c454abf6029a51a4e857225ffd900f8729f
邓如春 <1107151332@qq.com>
2024-04-16 22:37:17 +0800

    Initial commit

---
+++ assets/login-CONQYntM.js
@@ -0,0 +1,1 @@
+import{d as n,l as t,c as a,a as s,t as c,o as l,_ as i}from"./index-CuKPwBSU.js";const _={class:"column"},r=n({__name:"login",setup(u){const e=t(0);function o(){e.value++}return(d,p)=>(l(),a("div",_,[s("div",{onClick:o},"login "+c(e.value),1)]))}}),m=i(r,[["__file","login.vue"]]);export{m as default};
